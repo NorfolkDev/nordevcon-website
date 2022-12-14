d19972ce3c3c76b506223f7b53c329a58f4c101a
Tom Sherman <the.tomsherman@gmail.com>
2022-12-14 10:30:26 +0000

    Actually use the cache

--- pages/api/airtable.js
+++ pages/api/airtable.js
@@ -2,22 +2,33 @@ const apiKey = process.env.NEXT_PUBLIC_AIRTABLE_API_KEY;
 const apiBase = "https://api.airtable.com/v0/appsFsySYgjKqDoLu";
 
 async function get(endpoint, cache) {
-  const url = `${apiBase}${endpoint}`
-  return fetch(url, {
+  const url = `${apiBase}${endpoint}`;
+
+  const cached = await cache?.match(url);
+
+  if (cached) {
+    return extractJson(cached);
+  }
+
+  const res = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
     },
-  })
-    .then((res) => {
-      if (cache) {
-        const cloned = res.clone();
-        cloned.headers.set("Cache-Control", "max-age=60");
-        cache.put(url, cloned);
-      }
-      return res.json();
-    })
-    .then((json) => json.records);
+  });
+
+  if (cache) {
+    const cloned = res.clone();
+    cloned.headers.set("Cache-Control", "max-age=60");
+    cache.put(url, cloned);
+  }
+
+  return extractJson(res);
+}
+
+async function extractJson(res) {
+  const json = await res.json();
+  return json.records;
 }
 
 export async function getSchedule(cache) {
