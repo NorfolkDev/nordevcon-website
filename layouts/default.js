631ed976b7a4f013705c055f7421ce9cccdf23d2
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-05-02 13:54:40 +0100

    add note about archive site

--- layouts/default.js
+++ layouts/default.js
@@ -37,6 +37,10 @@ export default function Layout({ children, heading = null, hero = null }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
+      <div className="bg-white fixed top-0 left-0 right-0 z-50 p-4 font-bold">
+        <p>This is an archive of a previous conference - thanks for checking it out! You can see our current conference at <a class="underline" href="https://nordevcon.com">nordevcon.com</a></p>
+      </div>
+    
       <main className="min-h-screen bg-blue-50">{children}</main>
     </>
   );
