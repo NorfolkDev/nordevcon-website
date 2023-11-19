43019997e9fd80a6963c51f597317612da2e843a
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-11-19 18:15:18 +0000

    Laravel 10 foundations

---
+++ config/variables.php
@@ -0,0 +1,26 @@
+
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Site-Wide Variables
+    |--------------------------------------------------------------------------
+    |
+    | Commonly used descriptions, wordage, dateage, etc.
+     */
+
+    'title' => 'Norfolk Developers Conference 2024',
+
+    'short_title' => 'nor(DEV): con 2024',
+
+    'date' => '15th & 16th February 2024',
+
+    'description' => "Talks on language, business, methodology, & game development. Join us for two days of development goodness at The Kings' Centre.",
+
+    'ticket_url' => 'https://ti.to/norfolkdevelopers/nordevcon-24',
+
+    'brand_color' => '#44337a',
+
+];
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>@yield("title") | {{ config("variables.title") }}</title>
+        <meta
+            property="og:title"
+            content="@yield("title") | {{ config("variables.title") }}"
+        />
+        <meta
+            name="twitter:title"
+            content="@yield("title") | {{ config("variables.title") }}"
+        />
+
+        <meta
+            name="description"
+            content="@yield('description', config('variables.description'))"
+        />
+        <meta
+            property="og:description"
+            content="@yield('description', config('variables.description'))"
+        />
+        <meta
+            name="twitter:description"
+            content="@yield('description', config('variables.description'))"
+        />
+
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+            name="theme-color"
+            content="{{ config("variables.brand_color") }}"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        @stack("meta")
+
+        @googlefonts(["nonce" => csp_nonce()])
+        @vite("resources/css/app.css")
+    </head>
+
+    <body class="min-h-[100dvh]">
+        <main class="min-h-full">
+            @yield("content")
+        </main>
+
+        @vite("resources/js/app.js")
+    </body>
+</html>
