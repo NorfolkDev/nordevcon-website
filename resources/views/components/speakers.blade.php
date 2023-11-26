feaf07f38b983c021d1d570c7026fdc28bfc1600
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-11-26 18:10:57 +0000

    fix: remove debug

--- resources/views/components/speakers.blade.php
+++ resources/views/components/speakers.blade.php
@@ -1,6 +1,5 @@
 <div>
     <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-    @dump($speakers)
     <ol class="grid grid-cols-4">
         @foreach ($speakers as $speaker)
             <li>
