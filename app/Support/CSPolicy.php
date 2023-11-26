d2c2c4d0c3930d833ffc377d180474dc16ad4b32
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-11-26 15:11:39 +0000

    fix: extend CSP policy to allow images from sessionize

---
+++ app/Support/CSPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Support;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+
+class CSPolicy extends Basic
+{
+    public function configure()
+    {
+        parent::configure();
+
+        $this->addDirective(Directive::IMG, 'sessionize.com cache.sessionize.com');
+    }
+}
