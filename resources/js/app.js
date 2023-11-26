c7ea9b0b1bd12ecf7d4366e3344e7aed1aa6c4b1
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-11-26 13:25:43 +0000

    add Sessionize Service, with basic Sessions and Speakers Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-/* I'll have none of that :D */
+const dialogs = Array.from(document.querySelectorAll("button[dialog]"));
+
+console.log(dialogs);
+
+dialogs.map((dialog) =>
+    dialog.addEventListener("click", (event) =>
+        event.target.parentNode.querySelector("dialog").showModal()
+    )
+);
