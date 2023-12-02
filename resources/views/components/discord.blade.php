52d92f4f1efc42adc2bf3a9e4ea42f0c58fb0728
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-02 21:18:07 +0000

    feat: add discord card

---
+++ resources/views/components/discord.blade.php
@@ -0,0 +1,42 @@
+<div class="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-20">
+    <div
+        class="flex flex-col gap-8 overflow-hidden rounded-lg bg-[#5865F2] p-12 text-white shadow-lg lg:grid lg:grid-cols-4"
+    >
+        <div class="col-span-3 lg:self-center">
+            <h2 class="text-3xl tracking-tight sm:text-4xl">
+                <span class="block font-bold">
+                    Join the conversation ahead of time
+                </span>
+                <span class="mt-2 block">
+                    The “Conference Corridor experience” pre &amp; post
+                    conference.
+                </span>
+            </h2>
+            <p class="mt-4 text-lg font-bold text-indigo-100">
+                Our friendly, active and helpful Discord server. We discuss
+                topics related to software development, as well as share
+                resources, tips and tricks. Whether you're a beginner or a
+                professional, our members are here to help you out!
+            </p>
+            <a
+                href="https://nor.dev/discord"
+                class="mt-8 inline-flex items-center rounded-md border border-transparent bg-white px-12 py-3 text-lg font-bold text-indigo-600 shadow hover:bg-indigo-50"
+            >
+                Join the server
+            </a>
+        </div>
+        <div class="col-span-1 -mt-64 opacity-10 lg:mt-0 lg:opacity-100">
+            <div class="ml-auto max-w-xs rotate-12 drop-shadow-md">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 127.14 96.36"
+                >
+                    <path
+                        fill="currentColor"
+                        d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
+                    ></path>
+                </svg>
+            </div>
+        </div>
+    </div>
+</div>
