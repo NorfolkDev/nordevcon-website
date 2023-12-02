5e57365f28af79e0838f24a05f76ef31bd307df9
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-02 13:35:25 +0000

    feat: add caching to Sessionize service

--- app/Services/Sessionize.php
+++ app/Services/Sessionize.php
@@ -5,29 +5,38 @@ namespace App\Services;
 use App\Data\Sessions\Schedule;
 use Error;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class Sessionize
 {
     static function getSessions(): Schedule
     {
-        $response = Http::get("https://sessionize.com/api/v2/6rdd3z2a/view/GridSmart");
+        $data = Cache::remember("sessionize_sessions", 10_800, function () {
+            $response = Http::get("https://sessionize.com/api/v2/6rdd3z2a/view/GridSmart");
 
-        if (!$response->ok()) {
-            throw new Error("Error getting sessions from Sessionize");
-        }
+            if (!$response->ok()) {
+                throw new Error("Error getting sessions from Sessionize");
+            }
 
-        return Schedule::fromSessionize($response->json());
+            return $response->json();
+        });
+
+        return Schedule::fromSessionize($data);
     }
 
     static function getSpeakers(): Collection
     {
-        $response = Http::get("https://sessionize.com/api/v2/6rdd3z2a/view/Speakers");
+        $data = Cache::remember("sessionize_speakers", 10_800, function () {
+            $response = Http::get("https://sessionize.com/api/v2/6rdd3z2a/view/Speakers");
 
-        if (!$response->ok()) {
-            throw new Error("Error getting speakers from Sessionize");
-        }
+            if (!$response->ok()) {
+                throw new Error("Error getting speakers from Sessionize");
+            }
 
-        return collect($response->json());
+            return $response->json();
+        });
+
+        return collect($data);
     }
 }
