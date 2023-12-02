a627c1cf0ddf77960a5a8c0fa5516933f4d8551d
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-02 19:59:04 +0000

    feat: add introduction component from og site

---
+++ resources/views/components/introduction.blade.php
@@ -0,0 +1,48 @@
+<div class="relative">
+    <div
+        aria-hidden="true"
+        class="absolute hidden h-full w-1/2 bg-gray-100 lg:block"
+    ></div>
+    <div class="relative bg-gray-100 lg:bg-transparent">
+        <div
+            class="mx-auto max-w-7xl px-4 sm:px-6 lg:grid lg:grid-cols-2 lg:px-8"
+        >
+            <div
+                class="mx-auto max-w-2xl pb-12 pt-32 lg:max-w-none lg:pb-32 lg:pt-64"
+            >
+                <div class="lg:pr-16">
+                    <h2
+                        class="mb-8 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl"
+                    >
+                        Two days of talks on mobile, web, game, data science,
+                        <span class="italic">humans</span>
+                        , startups &amp; more.
+                    </h2>
+                    <p class="mb-2 text-2xl text-slate-900">
+                        Topics to interest and enthrall all; in a welcoming and
+                        inclusive atmosphere. Friendships to kindle, friends to
+                        meet, & connections to make!
+                    </p>
+
+                    <p class="text-xl text-slate-500">
+                        We'll gather in Norwich to learn, discuss, and network
+                        with an eclectic bunch of the best of East Anglia's tech
+                        scene, joined by a group of international conference
+                        veterans. We're bringing 35 sessions to The Kings'
+                        Centre, kicking off on the 23rd February 2023, and
+                        closing 24th February 2023
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div
+        class="h-48 w-full sm:h-64 lg:absolute lg:right-0 lg:top-0 lg:h-full lg:w-1/2"
+    >
+        <img
+            src="{{ Vite::asset("resources/img/audience.jpg") }}"
+            alt="An audience at nor(DEV): con, gaze fixed on the speaker."
+            class="h-full w-full object-cover object-center"
+        />
+    </div>
+</div>
