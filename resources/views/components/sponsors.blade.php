d44f0eef7c9a3f688a876f94ada1554a667326ff
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-03 14:17:20 +0000

    feat: update content

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -1,109 +1,138 @@
-<div class="bg-indigo-700">
-    <div class="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-20">
+<div class="bg-slate-900">
+    <div
+        class="mx-auto flex max-w-7xl flex-col gap-12 px-4 py-12 sm:px-6 lg:px-8 lg:py-20"
+    >
         <h2
-            class="mb-6 text-3xl font-bold tracking-tight text-indigo-100 lg:text-5xl"
+            class="text-5xl font-black tracking-wide text-wave-orange lg:text-6xl"
         >
             Sponsors
         </h2>
 
-        <p class="mb-12 text-center text-2xl text-indigo-100">
+        <p class="text-center text-2xl text-indigo-100">
             The Norfolk Developers Conference wouldn't be possible without the
             support of our wonderful sponsors. We're really grateful for their
             trust and support; and are proud to feature them below.
         </p>
 
-        <div class="flex flex-col gap-12">
-            <div class="text-wave-purple">
-                <p class="text-center text-2xl text-indigo-100">
-                    Would you like to Sponsor East Anglia's biggest tech
-                    conference? Packages starting at £500. Get in touch and
-                    let's talk about a conference sponsorship.
-                </p>
-            </div>
-
-            @foreach ($sponsors->where("package", "Elite") as $sponsor)
-                <div
-                    class="grid rounded-md bg-indigo-100 p-4 lg:grid-cols-2 lg:items-center lg:gap-6 lg:p-12"
-                >
-                    <div>
-                        <h2
-                            class="mb-6 text-3xl font-bold tracking-tight sm:text-4xl"
+        <ol class="flex flex-col gap-6">
+            <li>
+                <ol>
+                    @foreach ($sponsors->where("package", "Elite") as $sponsor)
+                        <li
+                            class="grid rounded-md bg-indigo-100 p-4 lg:grid-cols-2 lg:items-center lg:gap-6 lg:p-12"
                         >
-                            {{ $sponsor->name }}
-                        </h2>
+                            <div>
+                                <label
+                                    class="mb-4 inline-block rounded bg-wave-orange px-2 py-1 font-black text-white"
+                                >
+                                    {{ $sponsor->package }} Sponsor
+                                </label>
 
-                        <p>{{ $sponsor->description }}</p>
-                        <div class="mt-8 flex gap-4">
-                            <!-- Socials -->
-                        </div>
-                    </div>
-                    <div
-                        class="col-span-1 row-start-1 flex justify-center px-8 py-8 lg:row-start-auto"
-                    >
-                        <img
-                            class="object-contain"
-                            src="{{ $sponsor->logo }}"
-                            alt="Elite Sponsor, {{ $sponsor->name }}"
-                        />
-                    </div>
-                </div>
-            @endforeach
+                                <h2 class="mb-6 text-3xl font-bold sm:text-4xl">
+                                    {{ $sponsor->name }}
+                                </h2>
 
-            <div class="grid auto-rows-fr gap-6 lg:grid-cols-2">
-                @foreach ($sponsors->where("package", "Partner") as $sponsor)
-                    <div
-                        class="flex flex-col items-center rounded-md bg-indigo-100 px-8 py-8"
-                    >
-                        <img
-                            class="object-contain"
-                            src="{{ $sponsor->logo }}"
-                            alt="Partner Sponsor, {{ $sponsor->name }}"
-                        />
-
-                        <div class="mt-8 flex grow flex-col">
-                            <h3 class="mb-2 text-3xl font-bold">
-                                {{ $sponsor->name }}
-                            </h3>
-                            <div class="grow">
                                 <p>{{ $sponsor->description }}</p>
+                                <div class="mt-8 flex gap-4">
+                                    <!-- Socials -->
+                                </div>
                             </div>
-                            <div class="mt-8 flex gap-4">
-                                <!-- Socials -->
+                            <div
+                                class="col-span-1 row-start-1 flex justify-center px-8 py-8 lg:row-start-auto"
+                            >
+                                <img
+                                    class="object-contain"
+                                    src="{{ $sponsor->logo }}"
+                                    alt="Logo for Elite Sponsor, {{ $sponsor->name }}"
+                                />
                             </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+                        </li>
+                    @endforeach
+                </ol>
+            </li>
+            <li>
+                <ol class="grid auto-rows-fr gap-6 lg:grid-cols-2">
+                    @foreach ($sponsors->where("package", "Partner") as $sponsor)
+                        <li class="rounded-md bg-indigo-100 px-8 py-8">
+                            <label
+                                class="mb-4 inline-block rounded bg-wave-orange px-2 py-1 font-black text-white"
+                            >
+                                {{ $sponsor->package }} Sponsor
+                            </label>
 
-            <div class="grid auto-rows-fr grid-cols-2 gap-6 lg:grid-cols-4">
-                @foreach ($sponsors->where("package", "Associate") as $sponsor)
-                    <a
-                        href="{{ $sponsor->website }}"
-                        class="col-span-1 flex justify-center rounded-md bg-indigo-100 px-8 py-8"
-                    >
-                        <img
-                            class="object-contain"
-                            src="{{ $sponsor->logo }}"
-                            alt="Associate Sponsor, {{ $sponsor->name }}"
-                        />
-                    </a>
-                @endforeach
-            </div>
+                            <div class="flex grow flex-col">
+                                <h3 class="text-3xl font-bold">
+                                    {{ $sponsor->name }}
+                                </h3>
+                                <div class="py-4">
+                                    <img
+                                        class="mx-auto object-contain"
+                                        src="{{ $sponsor->logo }}"
+                                        alt="Logo for Partner Sponsor, {{ $sponsor->name }}"
+                                    />
+                                </div>
+                                <div class="grow">
+                                    <p>{{ $sponsor->description }}</p>
+                                </div>
+                                <div class="mt-8 flex gap-4">
+                                    <!-- Socials -->
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                </ol>
+            </li>
+            <li>
+                <ol class="grid auto-rows-fr grid-cols-2 gap-6 lg:grid-cols-4">
+                    @foreach ($sponsors->where("package", "Associate") as $sponsor)
+                        <li
+                            class="col-span-1 flex rounded-md bg-indigo-100 px-8 py-8"
+                        >
+                            <label class="sr-only">
+                                {{ $sponsor->package }} Sponsor
+                            </label>
+                            <a
+                                href="{{ $sponsor->website }}"
+                                class="flex justify-center"
+                            >
+                                <img
+                                    class="object-contain"
+                                    src="{{ $sponsor->logo }}"
+                                    alt="Logo for Associate Sponsor, {{ $sponsor->name }}"
+                                />
+                            </a>
+                        </li>
+                    @endforeach
+                </ol>
+            </li>
+        </ol>
 
-            <div class="mt-12 flex items-center justify-center gap-6">
-                <a
-                    href="{{ config("variables.sponsor_apply_url") }}"
-                    class="rounded-md border border-transparent bg-violet-800 px-8 py-4 text-xl font-bold text-indigo-100 hover:bg-violet-700"
-                >
-                    Apply to sponsor
-                </a>
-                <a
-                    href="/sponsorship"
-                    class="rounded-md border border-transparent bg-violet-600 px-8 py-4 text-xl font-bold text-indigo-100 hover:bg-violet-500"
-                >
-                    Read more
-                </a>
-            </div>
+        <p class="text-center text-2xl text-indigo-100">
+            Would you like to Sponsor East Anglia's biggest tech conference?
+            Packages starting at £500. Get in touch and let's talk about a
+            conference sponsorship.
+        </p>
+
+        <p class="text-center text-2xl text-indigo-100">
+            nor(DEV): com is on the lookout for sponsors to contribute to the
+            success of our innovative and inclusive tech conference. By becoming
+            a sponsor, you play a crucial role in our community-driven event,
+            helping us keep costs reasonable for attendees.
+        </p>
+
+        <div class="flex items-center justify-center gap-6">
+            <a
+                href="{{ config("variables.sponsor_apply_url") }}"
+                class="rounded-md border border-transparent bg-violet-800 px-8 py-4 text-xl font-bold text-indigo-100 hover:bg-violet-700"
+            >
+                Apply to sponsor
+            </a>
+            <a
+                href="/sponsorship"
+                class="rounded-md border border-transparent bg-violet-600 px-8 py-4 text-xl font-bold text-indigo-100 hover:bg-violet-500"
+            >
+                Read more
+            </a>
         </div>
     </div>
 </div>
