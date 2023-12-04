db0b130a9d56ce79656248e5d8ef5c9eb5cd25ce
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-04 17:14:33 +0000

    feat: add a what to expect component

---
+++ resources/views/components/expect.blade.php
@@ -0,0 +1,78 @@
+<div class="mx-auto my-12 w-full px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto w-full max-w-2xl text-slate-900 lg:max-w-none">
+        <h4 class="mb-4 text-5xl font-black tracking-wide lg:text-6xl">
+            What to expect
+        </h4>
+
+        <ol
+            class="my-6 divide-y-2 lg:my-12 lg:grid lg:grid-cols-2 lg:divide-y-0"
+        >
+            <li class="flex py-2">
+                <div>
+                    <x-illustration.community class="w-36" />
+                </div>
+                <div>
+                    <p class="mb-1 font-black">Chill Community Vibe</p>
+                    <p>
+                        Picture this: you walk in, and it's like joining a big
+                        group of friends who love tech just as much as you do.
+                        No fancy vibes, just a cool community excited to share
+                        and learn.
+                    </p>
+                </div>
+            </li>
+
+            <li class="flex py-2">
+                <div>
+                    <x-illustration.ensalada class="w-36" />
+                </div>
+                <div>
+                    <p class="mb-1 font-black">Lunch on the House</p>
+                    <p>
+                        We get it - talking tech can work up an appetite. We've
+                        got your back with some tasty lunches. It's not just
+                        about the food; it's a chance to kick back, chat, and
+                        maybe make some new pals.
+                    </p>
+                </div>
+            </li>
+            <li class="flex py-2">
+                <div>
+                    <x-illustration.presentation class="w-36" />
+                </div>
+                <div>
+                    <p class="mb-1 font-black">
+                        Engaging and Informative Talks
+                    </p>
+                    <p>
+                        Our sessions cover a diverse range of topics, from the
+                        latest technological advancements to practical insights
+                        into industry trends. Whether you're interested in
+                        coding, development methodologies, or the broader tech
+                        landscape, there's something for everyone.
+                    </p>
+                </div>
+            </li>
+            <li class="flex py-2">
+                <div>
+                    <x-illustration.social class="w-36" />
+                </div>
+                <div>
+                    <p class="mb-1 font-black">Networking... but *fun*</p>
+                    <p>
+                        No forced handshakes or awkward exchanges here. It's all
+                        about making connections in a way that feels natural.
+                        Chat with speakers, meet fellow attendees, and who knows
+                        - you might just find your next project buddy.
+                    </p>
+                </div>
+            </li>
+        </ol>
+        <p class="mb-6 py-2 font-condensed text-2xl lg:mb-12">
+            So, ditch the pretence, grab a snack, and let's make 2024 the chill
+            tech hangout you'll remember.
+            <br />
+            See you there! 🚀
+        </p>
+    </div>
+</div>
