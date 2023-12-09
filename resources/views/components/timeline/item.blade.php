a59e0698cbf421ed4cb26423bda318c2a0938ac1
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-09 17:45:17 +0000

    feat: add timeline featurette

---
+++ resources/views/components/timeline/item.blade.php
@@ -0,0 +1,30 @@
+@props([
+    "entry",
+])
+
+<li class="group relative flex gap-x-4">
+    <div
+        class="absolute -bottom-8 left-0 top-0 flex w-8 justify-center group-last:h-8"
+    >
+        <div class="w-1 bg-wave-orange/50"></div>
+    </div>
+    <div
+        class="relative flex h-8 w-8 flex-none items-center justify-center bg-gray-100"
+    >
+        <div
+            class="h-6 w-6 rounded-full bg-wave-orange ring-2 ring-wave-orange/75"
+        ></div>
+    </div>
+    <div class="flex-auto">
+        <h4 class="font-black">{{ $entry->headline }}</h4>
+        <time
+            datetime="{{ $entry->datetime->toIso8601String() }}"
+            class="mb-2 text-xs leading-5 text-slate-900"
+        >
+            {{ $entry->datetime->isoFormat("ddd, Do MMM h:mma") }}
+        </time>
+        <p class="font-condensed text-sm">
+            {{ $entry->description }}
+        </p>
+    </div>
+</li>
