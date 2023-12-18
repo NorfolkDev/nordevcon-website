884eb4965db327f0d8eca77678998a2173f82e75
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-18 16:16:49 +0000

    fix: add CSP directive for mailchimp newsletter

--- app/Support/CSPolicy.php
+++ app/Support/CSPolicy.php
@@ -13,5 +13,6 @@ class CSPolicy extends Basic
 
         $this->addDirective(Directive::IMG, 'sessionize.com cache.sessionize.com');
         $this->addDirective(Directive::IMG, 'norfolkdevelopers.s3.eu-west-2.amazonaws.com');
+        $this->addDirective(Directive::FORM_ACTION, 'https://dev.us16.list-manage.com');
     }
 }
