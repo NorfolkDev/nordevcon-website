129d522ff09b5c5af29f6ebdc565c5b8fa3b3f21
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-12-18 14:22:02 +0000

    feat: add speaker info to session modal

--- resources/views/components/schedule/dialog.blade.php
+++ resources/views/components/schedule/dialog.blade.php
@@ -36,19 +36,24 @@
         <div class="bg-slate-300 p-4 text-slate-900">
             <div class="mb-4">
                 <p class="text-xl">
-                    In the {{ $session->room }} on {{ $session->startsAt }}
+                    In the {{ $session->room }} on
+                    {{ $session->startsAt->isoFormat("ddd, Do MMM h:mma") }}
                 </p>
             </div>
             <div class="flex gap-4">
                 <div>
-                    <img
-                        class="w-24 rounded-full border-4 border-slate-900"
-                        alt="Alex Kearns is at nor(DEV): con 2023"
-                        src="https://norfolkdevelopers.s3.eu-west-2.amazonaws.com/nordevcon-website/2023/img/speaker_alex-kearns.jpg"
-                    />
+                    @foreach ($session->speakers as $speaker)
+                        <img
+                            @class(["w-24 rounded-full border-2 border-slate-900", "-ml-20" => ! $loop->first])
+                            alt="{{ $speaker->fullName }} is at nor(DEV): con 2024"
+                            src="{{ $speaker->profilePictureUrl }}"
+                        />
+                    @endforeach
                 </div>
                 <div>
-                    <h4 class="text-2xl font-bold tracking-tight"></h4>
+                    <h4 class="text-2xl font-bold tracking-tight">
+                        {{ $session->speakers->map->fullName->join(", ") }}
+                    </h4>
                 </div>
             </div>
         </div>
