f7b17d72f659e8b95cff0085479fdcae77f00dc3
Alex Scotton <alex@alexscotton.com>
2025-08-03 12:12:12 +0100

    feat: add the archive banner

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -42,6 +42,17 @@
     </head>
 
     <body class="min-h-[100dvh] bg-gray-100">
+        <div
+            class="fixed left-0 right-0 top-0 z-[9999] bg-white/80 p-4 font-bold"
+        >
+            <p>
+                This is an archive of a previous conference - thanks for
+                checking it out! You can see our current conference at
+                <a class="underline" href="https://nordevcon.com">
+                    nordevcon.com
+                </a>
+            </p>
+        </div>
         <main>
             @yield("content")
         </main>
