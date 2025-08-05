1bf30f114c9a84ced21046e17f9337e4c5d2062b
Alex Scotton <alex@alexscotton.com>
2025-08-05 17:21:06 +0100

    fix: quick and dirty fix for mobile nav

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,10 +1,10 @@
 <div
-    {{ $attributes->merge(["class" => "flex items-center text-white divide-x divide-wave-orange"]) }}
+    {{ $attributes->merge(["class" => "flex flex-col md:flex-row text-white divide-y divide-wave-orange md:divide-y-0 md:divide-x md:divide-wave-orange"]) }}
 >
     @unless (request()->is("/"))
         <a
             href="/"
-            class="px-4 text-lg font-bold transition-colors hover:text-wave-orange"
+            class="w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center"
         >
             Home
         </a>
@@ -12,31 +12,31 @@
 
     <a
         href="/about"
-        @class(["px-4 text-lg font-bold transition-colors hover:text-wave-orange", "text-wave-orange" => request()->is("about")])
+        @class(["w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center", "text-wave-orange" => request()->is("about")])
     >
         About Us
     </a>
     <a
         href="/conference"
-        @class(["px-4 text-lg font-bold transition-colors hover:text-wave-orange", "text-wave-orange" => request()->is("conference")])
+        @class(["w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center", "text-wave-orange" => request()->is("conference")])
     >
         What to expect?
     </a>
     <a
         href="/attend"
-        @class(["px-4 text-lg font-bold transition-colors hover:text-wave-orange", "text-wave-orange" => request()->is("attend")])
+        @class(["w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center", "text-wave-orange" => request()->is("attend")])
     >
         Attend
     </a>
     <a
         href="/sponsor"
-        @class(["px-4 text-lg font-bold transition-colors hover:text-wave-orange", "text-wave-orange" => request()->is("sponsor")])
+        @class(["w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center", "text-wave-orange" => request()->is("sponsor")])
     >
         Sponsor
     </a>
     <a
         href="https://nor.dev/posts"
-        class="px-4 text-lg font-bold transition-colors hover:text-wave-orange"
+        class="w-full px-4 py-1 md:py-0 text-sm md:text-lg font-bold transition-colors hover:text-wave-orange text-left md:text-center"
     >
         News
     </a>
--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
     <div>
-        <div class="flex justify-end py-4 bg-slate-900">
+        <div class="flex justify-start md:justify-end py-2 md:py-4 bg-slate-900">
             <x-navigation />
         </div>
 
