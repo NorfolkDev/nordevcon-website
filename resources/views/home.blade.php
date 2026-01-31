0fee3ae8bdea5e4b06d1a07b7384501b50766dd0
Alex Scotton <alex@alexscotton.com>
2026-01-31 14:16:07 +0000

    feat: put schedule live

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,31 +10,8 @@
     <x-expect />
     <x-testimonials />
     <x-gallery />
-
     <x-tickets />
-
-    <div
-        class="relative mx-auto my-12 w-full px-4 sm:px-6 lg:max-w-7xl lg:px-8"
-    >
-        <h4 class="mb-4 text-3xl font-black tracking-wide lg:mb-8 lg:text-6xl">
-            The Schedule
-        </h4>
-
-        <div>
-            <div class="rounded-lg bg-white px-4 py-2">
-                <h4>
-                    Coming Soon...
-                    <a class="text-wave-purple underline" href="#newsletter">
-                        Subscribe to our newsletter
-                    </a>
-                    to be the first to know when it's announced!
-                </h4>
-            </div>
-        </div>
-    </div>
-
-    {{-- <x-schedule /> --}}
-
+    <x-schedule />
     <x-sponsors />
     <x-discord />
     <x-newsletter />
