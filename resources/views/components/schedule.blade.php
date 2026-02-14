72b0eb5e3c173e34a957ac620d2771f2cf972835
Alex Scotton <alex@alexscotton.com>
2026-02-14 12:25:16 +0000

    vibes: star animation

--- resources/views/components/schedule.blade.php
+++ resources/views/components/schedule.blade.php
@@ -73,13 +73,24 @@
                             </button>
                             @unless ($session->isServiceSession)
                                 <button
-                                    class="text-yellow-500"
+                                    class="relative text-yellow-500"
                                     data-session-id="{{ $session->id }}"
-                                    @click="favourites.includes('{{ $session->id }}') ? favourites.splice(favourites.indexOf('{{ $session->id }}'), 1) : favourites.push('{{ $session->id }}')"
+                                    @click="
+                                        let adding = !favourites.includes('{{ $session->id }}');
+                                        favourites.includes('{{ $session->id }}')
+                                            ? favourites.splice(favourites.indexOf('{{ $session->id }}'), 1)
+                                            : favourites.push('{{ $session->id }}');
+                                        if (adding) {
+                                            let overlay = $el.querySelector('[js--star-explode]');
+                                            overlay.classList.remove('animate-star-explode');
+                                            void overlay.offsetWidth;
+                                            overlay.classList.add('animate-star-explode');
+                                        }
+                                    "
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        class="h-6 w-6"
+                                        class="h-6 w-6 transition-[fill] duration-300"
                                         :fill="favourites.includes('{{ $session->id }}') ? 'currentColor' : 'none'"
                                         viewBox="0 0 24 24"
                                         stroke="currentColor"
@@ -89,6 +100,16 @@
                                             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                                             stroke-linecap="round"
                                             stroke-linejoin="round"
+                                        ></path>
+                                    </svg>
+                                    <svg
+                                        js--star-explode
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="pointer-events-none absolute inset-0 h-6 w-6 fill-current opacity-0"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                                         ></path>
                                     </svg>
                                 </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ export default {
                 sans: ["Montserrat", ...fontFamily.sans],
                 condensed: ["Roboto Condensed", ...fontFamily.sans],
             },
+            keyframes: {
+                'star-explode': {
+                    '0%':   { transform: 'scale(1)',   opacity: '1' },
+                    '50%':  { transform: 'scale(4)',   opacity: '0.5' },
+                    '100%': { transform: 'scale(5)', opacity: '0' },
+                },
+            },
+            animation: {
+                'star-explode': 'star-explode 0.4s ease-out forwards',
+            },
         },
     },
     plugins: [
